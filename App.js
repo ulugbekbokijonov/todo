0eb15c6df59a2314b24b6727b8e17121d1f8b7d8
Bokijonov Ulugbek <52455622+ulugbekbokijonov@users.noreply.github.com>
2021-01-21 15:33:11 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppToDo from './components/AppToDo'
+
+const App = () => {
+    return (
+        <div>
+            <AppToDo />
+        </div>
+    )
+}
+
+export default App
